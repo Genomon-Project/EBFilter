5732cb3478bddc0c5a6ff282933fa1a17b074c68
friend1ws <friend1ws@gmail.com>
2015-07-14 17:11:43 +0900

    fit_beta_binomial for estimating parameters

--- lib/ebfilter/beta_binomial.py
+++ lib/ebfilter/beta_binomial.py
@@ -2,14 +2,23 @@
 
 import math
 import numpy
+import scipy.optimize
 
-def beta_binomial_loglikelihood(params, data):
+def beta_binomial_loglikelihood(params, As, Bs):
+
+    """Calculating log-likelihood of beta-binomial distribution
+
+    Args:
+        params (List[float]): the parameter of beta distribution ([alpha, beta])
+    
+        As (numpy.array([int])): the counts for success
+        
+        Bs (numpy.array([int])): the counts of trials
+
+    """
 
     alpha = params[0]    
     beta = params[1]
-
-    As = numpy.array(data[0::2])
-    Bs = numpy.array(data[1::2])
 
     ML = 0
     ML = ML + reduce(lambda a, b: a + math.lgamma(b), numpy.r_[0, As + Bs + 1])
@@ -22,7 +31,32 @@ def beta_binomial_loglikelihood(params, data):
 
     ML = ML + len(As) * (math.lgamma(alpha + beta) - math.lgamma(alpha) - math.lgamma(beta))
 
-    return(ML)
+
+    # Here, we set the penalty term of alpha and beta (0.5 is slightly arbitray...)
+    ML = ML - 0.5 * math.log(alpha + beta)
+    return(-ML)
 
  
+
+def fit_beta_binomial(As, Bs):
+
+    """Obtaining maximum likelihood estimator of beta-binomial distribution
+
+    Args:
+        As (numpy.array([int])): the counts for success
+        
+        Bs (numpy.array([int])): the counts of trials
+
+    """
+
+    result = scipy.optimize.fmin_l_bfgs_b(beta_binomial_loglikelihood,
+                                          [20, 20],
+                                          args = (As, Bs),
+                                          approx_grad = True,
+                                          bounds = [(0.1, 10000000), (1, 10000000)])
+
+    return result[0]
+
+
+
 
