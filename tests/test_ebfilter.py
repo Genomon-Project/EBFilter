bbdca0f2146f5b26abcf6f0df6440f7a371ae330
ken0-1n <kchiba@hgc.jp>
2020-05-15 16:06:25 +0900

    add travis.yaml

--- tests/test_ebfilter.py
+++ tests/test_ebfilter.py
@@ -8,14 +8,18 @@ import subprocess
 class TestEBFilter(unittest.TestCase):
 
     def test1(self):
+        subprocess.check_call(["EBFilter", "--version"])
+
+    def test2(self):
+        subprocess.check_call(["samtools", "--version"])
+
+    def test3(self):
         cur_dir = os.path.dirname(os.path.abspath(__file__))
         tmp_dir = tempfile.mkdtemp()
         in_anno = cur_dir + "/../testdata/input.anno"
         in_bam = cur_dir + "/../testdata/tumor.bam"
-        # output_file = tmp_dir+"/output.anno"
-        output_file = cur_dir+"/../output.anno"
+        output_file = tmp_dir+"/output.anno"
         control_panel = tmp_dir + "/list_normal_sample.txt"
-        # control_panel = cur_dir + "/../list_normal_sample.txt"
         with open (control_panel, "w") as hout:
             print(cur_dir + "/../testdata/normalreference1.bam", file=hout)
             print(cur_dir + "/../testdata/normalreference2.bam", file=hout)
@@ -29,18 +33,18 @@ class TestEBFilter(unittest.TestCase):
             print(cur_dir + "/../testdata/normalreference10.bam", file=hout)
 
         subprocess.check_call(["EBFilter", "-f", "anno", in_anno, in_bam, control_panel, output_file])
+        subprocess.check_call(["cat", output_file])
 
         answer_file = cur_dir + "/../testdata/output.golden.anno"
         self.assertTrue(filecmp.cmp(output_file, answer_file, shallow=False))
         shutil.rmtree(tmp_dir)
 
-    def test2(self):
+    def test4(self):
         cur_dir = os.path.dirname(os.path.abspath(__file__))
         tmp_dir = tempfile.mkdtemp()
         in_vcf = cur_dir + "/../testdata/input.vcf.gz"
         in_bam = cur_dir + "/../testdata/tumor.bam"
-        # output_file = tmp_dir+"/output.vcf"
-        output_file = cur_dir+"/../output.vcf"
+        output_file = tmp_dir+"/output.vcf"
         control_panel = tmp_dir + "/list_normal_sample.txt"
         with open (control_panel, "w") as hout:
             print(cur_dir + "/../testdata/normalreference1.bam", file=hout)
